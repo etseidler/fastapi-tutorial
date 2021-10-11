e0a5cec15334d56ec292fc66b71b6eb05acfc436
Eric Seidler <eric.seidler@gmail.com>
2021-10-11 12:48:50 -0500

    query parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -15,7 +16,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
+# @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
@@ -44,3 +45,52 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item_query_param(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# @app.get("/items/{item_id}")
+async def read_item_optional_param(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+# @app.get("/items/{item_id}")
+async def read_item_boolean_param(
+    item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/items/{item_id}")
+async def read_user_item_required_query_param(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
