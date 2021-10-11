9224ea2001efb5eedb3e9791689bde5cfa81c62a
Eric Seidler <eric.seidler@gmail.com>
2021-10-11 13:09:09 -0500

    marking deprecated params

--- main.py
+++ main.py
@@ -121,6 +121,7 @@ async def read_items_param_validation(
         title="Query string",
         description="Query string for the items to search in the database that have a good match",
         min_length=3,
+        deprecated=True,
     )
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
